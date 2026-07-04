e4d7a82785c4348507e77ab418aa9983f08bfc54
vitaliy4us <vitaliy4us@yahoo.com>
2026-07-04 21:54:52 +0300

    Argos token was added in playwright config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,6 +19,7 @@ export default defineConfig<TestOptions>({
       {
         // Upload to Argos on CI only.
         uploadToArgos: !!process.env.CI,
+        token: "argos_wvp19cpykzqlv598bbcyjjvz6ni1x3xbm6"
       },
     ],
     ['json', {outputFile: 'test-results/jsonReport.json'}],
