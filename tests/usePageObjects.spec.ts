b395f0e59410d9f176736aa7e732a7a647de6967
vitaliy4us <vitaliy4us@yahoo.com>
2026-07-04 21:09:39 +0300

    added argos ci configuration

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,6 +1,7 @@
 import {test, expect} from '@playwright/test'
 import { PageManager } from '../page-objects/pageManager'
 import {faker} from '@faker-js/faker'
+import { argosScreenshot } from "@argos-ci/playwright"
 
 
 
@@ -37,5 +38,7 @@ test('parametrized methods @smoke', async({page}) => {
 test.only('testing with argos ci', async({page}) => {
     const pm = new PageManager(page)
     await pm.navigateTo().formLayoutsPage()
+    await argosScreenshot(page, "form layouts page")
     await pm.navigateTo().datepickerPage()
+    await argosScreenshot(page, "datepicker page")
 })
